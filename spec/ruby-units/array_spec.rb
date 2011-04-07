4322239a483124b2ebfc4ed2b0c93dee186c0139
Kevin Olbrich <kevin.olbrich@gmail.com>
2011-04-06 21:54:48 -0400

    add rake task to run specs, build basic specs for Array

---
+++ spec/ruby-units/array_spec.rb
@@ -0,0 +1,13 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Array do
+  
+  subject { [1, 'cm'] }
+  
+  it {should be_kind_of Array}
+  it {should respond_to :to_unit}
+  
+  specify { subject.to_unit.should == "1 cm".to_unit }
+  specify { subject.to_unit('mm').should == "10 mm".to_unit}
+  
+end
