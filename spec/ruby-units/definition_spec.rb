457c9631bb705c2c5da17056e3cf6267f186df30
Kevin Olbrich <kevin.olbrich@gmail.com>
2011-12-10 10:06:53 -0500

    add specs for display name

--- spec/ruby-units/definition_spec.rb
+++ spec/ruby-units/definition_spec.rb
@@ -3,8 +3,9 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe "Unit::Definition('eV')" do
   subject {
       Unit::Definition.new("eV") do |ev|
-        ev.aliases    = ["eV", "electron-volt"]
-        ev.definition = Unit("1.602E-19 joule")
+        ev.aliases      = ["eV", "electron-volt"]
+        ev.definition   = Unit("1.602E-19 joule")
+        ev.display_name = "electron-volt"
       end
       }
   
@@ -13,5 +14,5 @@ describe "Unit::Definition('eV')" do
   its(:scalar)        {should == 1.602E-19}
   its(:numerator)     {should == %w{<meter> <meter> <kilogram>}}
   its(:denominator)   {should == %w{<second> <second>}}
-  its(:display_name)  {should == "eV"}
+  its(:display_name)  {should == "electron-volt"}
 end
