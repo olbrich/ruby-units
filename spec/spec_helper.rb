e0a856efa06a9968a980960f31b5849663ad8f6a
Kevin Olbrich <kevin.olbrich@gmail.com>
2011-12-18 10:42:54 -0500

    add support for simplecov code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,16 @@ require 'rubygems'
 require 'bundler'
 Bundler.setup(:development)
 require 'rspec/core'
+
+# Initiate code coverage generation when needed
 begin
   require 'simplecov'
-  SimpleCov.start if ENV['SIMPLECOV']
+  SimpleCov.start do
+    add_filter "/spec/"
+    add_filter "/test/"
+    skip_token "nocov_19"
+  end if ENV['COVERAGE']
 rescue LoadError
 end
+
 require File.dirname(__FILE__) + "/../lib/ruby-units"
