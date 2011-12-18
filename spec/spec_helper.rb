570b3d1c56e8ff8a2a355136261a2cecaf01d537
Kevin Olbrich <kevin.olbrich@gmail.com>
2011-12-18 00:31:03 -0500

    add support for simplecov.  Fixes #32

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,9 @@ require 'rubygems'
 require 'bundler'
 Bundler.setup(:development)
 require 'rspec/core'
+begin
+  require 'simplecov'
+  SimpleCov.start if ENV['SIMPLECOV']
+rescue LoadError
+end
 require File.dirname(__FILE__) + "/../lib/ruby-units"
