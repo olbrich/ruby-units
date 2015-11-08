c1406d354c3f0e50954d203b8e87003de67d8f78
Kevin Olbrich <kevin.olbrich@mckesson.com>
2015-11-07 22:21:57 -0500

    housekeeping

--- Gemfile
+++ Gemfile
@@ -1,17 +1,15 @@
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 group :development do
   gem 'bundler', '~> 1.0'
   gem 'jeweler'
-  gem 'pry-byebug', :platforms => :ruby_20
+  gem 'pry-byebug'
 end
 
 group :test do
   gem 'rake'
-  gem 'rcov', :platforms => :mri_18
-  gem 'simplecov', :require => false, :platforms => :mri_19
-  gem 'simplecov-html', :platforms => :mri_19
+  gem 'simplecov'
+  gem 'simplecov-html'
   gem 'rspec'
   gem 'guard-rspec'
 end
-
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,24 +1,19 @@
 require 'rubygems'
-require 'bundler'
-Bundler.setup(:development)
+require 'bundler/setup'
+Bundler.require(:development, :test)
 require 'rspec/core'
 
-# Initiate code coverage generation when needed
-begin
-  require 'simplecov'
-  SimpleCov.start do
-    add_filter "/spec/"
-    add_filter "/test/"
-    skip_token "nocov_19"
-  end if ENV['COVERAGE']
-rescue LoadError
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/test/'
+  skip_token 'nocov_19'
 end
 
 RSpec.configure do |config|
-  #config.order = :random
+  config.order = :random
   config.filter_run_including focus: true
   config.run_all_when_everything_filtered = true
 end
 
 require File.dirname(__FILE__) + "/../lib/ruby-units"
-
