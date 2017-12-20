b8833c1663fcef23a43600ebfcda330d363b95ae
Kevin Olbrich <kevin.olbrich@mckesson.com>
2017-12-20 14:56:31 -0500

    parser for metric sytle units that will handle most normal cases.  Does not yet dynamically determine the list of prefixes and unit names to search for

--- spec/ruby_units/parsers/metric_spec.rb
+++ spec/ruby_units/parsers/metric_spec.rb
@@ -29,5 +29,95 @@ RSpec.describe RubyUnits::Parsers::Metric do
     expect(subject.zero).to parse('0')
   end
 
+  it 'parses digits' do
+    expect(subject.digits).to_not parse('')
+    expect(subject.digits).to parse('0')
+    expect(subject.digits).to parse('1')
+    expect(subject.digits).to parse('2')
+    expect(subject.digits).to parse('3')
+    expect(subject.digits).to parse('4')
+    expect(subject.digits).to parse('5')
+    expect(subject.digits).to parse('6')
+    expect(subject.digits).to parse('7')
+    expect(subject.digits).to parse('8')
+    expect(subject.digits).to parse('9')
+    expect(subject.digits).to parse('12345') # more than one digit
+    expect(subject.digits).to parse('0000012345') # can have leading zeros
+  end
 
+  it 'parses integers with separators' do
+    expect(subject.integer_with_separators).to parse('1,000')
+    expect(subject.integer_with_separators).to parse('10,000')
+    expect(subject.integer_with_separators).to parse('100,000')
+    expect(subject.integer_with_separators).to parse('1,000,000')
+    expect(subject.integer_with_separators).to_not parse('10,00,00')
+    expect(subject.integer_with_separators).to parse('1_000')
+    expect(subject.integer_with_separators).to parse('10_000')
+    expect(subject.integer_with_separators).to parse('100_000')
+    expect(subject.integer_with_separators).to parse('1_000_000')
+    expect(subject.integer_with_separators).to parse('12,345')
+    expect(subject.integer_with_separators).to_not parse('1000_000')
+  end
+
+  it 'parses decimals' do
+    expect(subject.decimal).to parse('1234.567')
+    expect(subject.decimal).to parse('+1234.567')
+    expect(subject.decimal).to parse('-1234.567')
+    expect(subject.decimal).to parse('0.567')
+    expect(subject.decimal).to parse('12,345.56')
+  end
+
+  it 'parses rationals' do
+    expect(subject.rational).to parse('1/2')
+    expect(subject.rational).to parse('-1/2')
+    expect(subject.rational).to parse('+1/2')
+    expect(subject.rational).to parse('1/0.5')
+    expect(subject.rational).to parse('1.5/3.0')
+  end
+
+  it 'parses mixed fractions' do
+    expect(subject.mixed_fraction).to parse('1 3/4')
+    expect(subject.mixed_fraction).to parse('1-3/4')
+    expect(subject.mixed_fraction).to parse('-1 3/4')
+  end
+
+  it 'parses scientific notation' do
+    expect(subject.scientific).to parse('1.23E45')
+    expect(subject.scientific).to parse('+1.23E45')
+    expect(subject.scientific).to parse('-1.23E45')
+    expect(subject.scientific).to parse('1.23E+45')
+    expect(subject.scientific).to parse('1.23E-45')
+  end
+
+  it 'parses complex numbers' do
+    expect(subject.complex).to parse('1+1i')
+    expect(subject.complex).to parse('1-1i')
+    expect(subject.complex).to parse('-1+1i')
+    expect(subject.complex).to parse('-1-1i')
+  end
+
+  it 'parses scalars' do
+    expect(subject.scalar).to parse('1')
+    expect(subject.scalar).to parse('1.5')
+    expect(subject.scalar).to parse('1 1/2')
+    expect(subject.scalar).to parse('1+1i')
+  end
+
+  it 'parses unit_atom' do
+    expect(subject.unit_atom).to parse('m')
+    expect(subject.unit_atom).to parse('mm')
+    expect(subject.unit_atom).to parse('1 m')
+    expect(subject.unit_atom).to parse('1 mm')
+    expect(subject.unit_atom).to parse('1 mm^-2')
+    expect(subject.unit_atom).to parse('1 mm^0.5')
+    expect(subject.unit_atom).to parse('1 mm^2')
+    expect(subject.unit_atom).to parse('1 mm**-2')
+    expect(subject.unit_atom).to parse('1 mm**0.5')
+    expect(subject.unit_atom).to parse('1 mm**2')
+  end
+
+  it 'parses a unit' do
+    expect(subject.unit).to parse('1 mm/s/s')
+    expect(subject.unit).to parse('1 m*m/s/s')
+  end
 end
