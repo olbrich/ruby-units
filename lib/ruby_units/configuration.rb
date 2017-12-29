3aa0e5e8ecdcac260228405d5bfbeec8f608634c
Kevin Olbrich <kevin.olbrich@mckesson.com>
2017-12-29 11:14:03 -0500

    add logger

--- lib/ruby_units/configuration.rb
+++ lib/ruby_units/configuration.rb
@@ -1,9 +1,11 @@
+require 'logger'
 # allow for optional configuration of RubyUnits
 #
 # Usage:
 #
 #     RubyUnits.configure do |config|
 #       config.separator = false
+#       config.logger = Logger.new(STDOUT)
 #     end
 module RubyUnits
   class << self
@@ -27,15 +29,21 @@ module RubyUnits
     # used to separate the scalar from the unit when generating output.
     # set to nil to prevent adding a space to the string representation of a unit
     # separators other than ' ' and '' may work, but you may encounter problems
-    attr_reader :separator
+    attr_reader :separator, :logger
 
     def initialize
       self.separator = true
+      self.logger = Logger.new(STDOUT)
+      self.logger.level = Logger::INFO
     end
 
     def separator=(value)
       raise ArgumentError, "configuration 'separator' may only be true or false" unless value.class == TrueClass || value.class == FalseClass
       @separator = value ? ' ' : nil
+    end
+
+    def logger=(value)
+      @logger=value
     end
   end
 end
