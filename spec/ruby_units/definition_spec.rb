3c981c55f3366f9aa7e777a7993a226db3621a96
Kevin Olbrich <kevin.olbrich@gmail.com>
2018-09-23 11:20:12 -0400

    formatting

--- spec/ruby_units/definition_spec.rb
+++ spec/ruby_units/definition_spec.rb
@@ -1,11 +1,11 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 # Meter is a base unit so the definition is simple
-describe "definition of meter" do
+describe 'definition of meter' do
   subject(:meter) { RubyUnits::Unit.definition('meter') }
   specify do
     expect(meter).to be_a RubyUnits::Unit::Definition
-    expect(meter.aliases).to eq ["m", "meter", "meters", "metre", "metres"]
+    expect(meter.aliases).to eq %w[m meter meters metre metres]
     expect(meter.base?).to eq true
     expect(meter.complexity).to eq 1
     expect(meter.denominator).to eq RubyUnits::Unit::UNITY_ARRAY
@@ -21,11 +21,11 @@ describe "definition of meter" do
 end
 
 # Inch is defined in terms of meters
-describe "definition of inch" do
+describe 'definition of inch' do
   subject(:inch) { RubyUnits::Unit.definition('inch') }
   specify do
     expect(inch).to be_a RubyUnits::Unit::Definition
-    expect(inch.aliases).to eq ["in", "inch", "inches", "\""]
+    expect(inch.aliases).to eq ['in', 'inch', 'inches', '"']
     expect(inch.base?).to eq false # because it's defined in terms of meters
     expect(inch.complexity).to eq 1
     expect(inch.denominator).to eq RubyUnits::Unit::UNITY_ARRAY
@@ -34,18 +34,18 @@ describe "definition of inch" do
     expect(inch.name).to eq '<inch>'
     expect(inch.numerator).to eq ['<meter>']
     expect(inch.prefix?).to eq false
-    expect(inch.scalar).to eq Rational(127,5000)
+    expect(inch.scalar).to eq Rational(127, 5000)
     expect(inch.signature).to eq RubyUnits::Unit::LENGTH
     expect(inch.system).to eq :imperial
   end
 end
 
 # sqft is defined as the square of another unit
-describe "definition of sqft" do
+describe 'definition of sqft' do
   subject(:sqft) { RubyUnits::Unit.definition('sqft') }
   specify do
     expect(sqft).to be_a RubyUnits::Unit::Definition
-    expect(sqft.aliases).to eq ["sqft"]
+    expect(sqft.aliases).to eq ['sqft']
     expect(sqft.base?).to eq false
     expect(sqft.complexity).to eq 2
     expect(sqft.denominator).to eq RubyUnits::Unit::UNITY_ARRAY
@@ -54,7 +54,7 @@ describe "definition of sqft" do
     expect(sqft.name).to eq '<sqft>'
     expect(sqft.numerator).to eq ['<meter>', '<meter>']
     expect(sqft.prefix?).to eq false
-    expect(sqft.scalar).to eq Rational(145161,1562500)
+    expect(sqft.scalar).to eq Rational(145_161, 1_562_500)
     expect(sqft.signature).to eq RubyUnits::Unit::LENGTH**2
     expect(sqft.system).to eq :imperial
   end
@@ -62,11 +62,11 @@ end
 
 # kph is a rate (LENGTH/TIME) so it's defined in terms of a unit divided by
 # another
-describe "definition of kph" do
+describe 'definition of kph' do
   subject(:kph) { RubyUnits::Unit.definition('kph') }
   specify do
     expect(kph).to be_a RubyUnits::Unit::Definition
-    expect(kph.aliases).to eq ["kph"]
+    expect(kph.aliases).to eq ['kph']
     expect(kph.base?).to eq false
     expect(kph.complexity).to eq 2
     expect(kph.denominator).to eq ['<second>']
@@ -82,11 +82,11 @@ describe "definition of kph" do
 end
 
 # Hz is a frequency (1/TIME) so it's defined in terms of unity divided by time
-describe "definition of Hz" do
+describe 'definition of Hz' do
   subject(:hz) { RubyUnits::Unit.definition('hertz') }
   specify do
     expect(hz).to be_a RubyUnits::Unit::Definition
-    expect(hz.aliases).to eq ["Hz", "hertz"]
+    expect(hz.aliases).to eq %w[Hz hertz]
     expect(hz.base?).to eq false
     expect(hz.complexity).to eq 1
     expect(hz.denominator).to eq ['<second>']
@@ -102,11 +102,11 @@ describe "definition of Hz" do
 end
 
 # percent is a unitless ratio
-describe "definition of percent" do
+describe 'definition of percent' do
   subject(:percent) { RubyUnits::Unit.definition('percent') }
   specify do
     expect(percent).to be_a RubyUnits::Unit::Definition
-    expect(percent.aliases).to eq ["%", "percent"]
+    expect(percent.aliases).to eq ['%', 'percent']
     expect(percent.base?).to eq false
     expect(percent.complexity).to eq 0
     expect(percent.denominator).to eq RubyUnits::Unit::UNITY_ARRAY
@@ -115,13 +115,13 @@ describe "definition of percent" do
     expect(percent.name).to eq '<percent>'
     expect(percent.numerator).to eq RubyUnits::Unit::UNITY_ARRAY
     expect(percent.prefix?).to eq false
-    expect(percent.scalar).to eq Rational(1,100)
+    expect(percent.scalar).to eq Rational(1, 100)
     expect(percent.signature).to eq RubyUnits::Unit::UNITLESS
     expect(percent.system).to be_nil # percent is not part of any unit system
   end
 end
 
-describe "new unit definition for electron volt (not normally definied)" do
+describe 'new unit definition for electron volt (not normally definied)' do
   subject do
     Unit::Definition.new('eV') do |ev|
       ev.aliases      = ['eV', 'electron-volt']
