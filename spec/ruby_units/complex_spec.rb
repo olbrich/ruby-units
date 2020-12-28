81794850218631f84fe6335049ef45967f844be4
Kevin Olbrich <kevin.olbrich@gmail.com>
2020-12-28 11:54:14 -0500

    Complex comparisons work differently in Ruby 2.7 Fixes #213

--- spec/ruby_units/complex_spec.rb
+++ spec/ruby_units/complex_spec.rb
@@ -1,14 +1,17 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-# Complex numbers are a bit strange
-# Technically you can't really compare them using <=>, and ruby 1.9 does not implement this method for them
-# so it stands to reason that complex units should also not support :> or :<
-
 describe Complex do
   subject { Complex(1, 1) }
   it { is_expected.to respond_to :to_unit }
 end
 
+# Complex numbers are a bit strange. Technically you can't really compare them
+# using :<=>, and Ruby < 2.7 does not implement this method for them so it stands
+# to reason that complex units should also not support :> or :<.
+#
+# This inconsistency was corrected in Ruby 2.7.
+# @see https://rubyreferences.github.io/rubychanges/2.7.html#complex
+# @see https://bugs.ruby-lang.org/issues/15857
 describe 'Complex Unit' do
   subject { Complex(1.0, -1.0).to_unit }
 
@@ -19,7 +22,12 @@ describe 'Complex Unit' do
   it { is_expected.to be === '1-1i'.to_unit }
 
   it 'is not comparable' do
-    expect { subject > '1+1i'.to_unit }.to raise_error(NoMethodError)
-    expect { subject < '1+1i'.to_unit }.to raise_error(NoMethodError)
+    if Complex.respond_to?(:<=>) # this is true for Ruby >= 2.7
+      expect { subject > '1+1i'.to_unit }.to raise_error(ArgumentError)
+      expect { subject < '1+1i'.to_unit }.to raise_error(ArgumentError)
+    else
+      expect { subject > '1+1i'.to_unit }.to raise_error(NoMethodError)
+      expect { subject < '1+1i'.to_unit }.to raise_error(NoMethodError)
+    end
   end
 end
