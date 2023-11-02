ccae37dd54cab89cd974b7cb487c1b8c240a872a
Kevin Olbrich <kevin.olbrich@gmail.com>
2023-11-02 07:21:25 -0400

    Allow for underscores in unit names and aliases. (#331)
    Fixes #330

--- spec/ruby_units/definition_spec.rb
+++ spec/ruby_units/definition_spec.rb
@@ -1,9 +1,9 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require_relative '../spec_helper'
 
 describe "Unit::Definition('eV')" do
   subject do
     Unit::Definition.new('eV') do |ev|
-      ev.aliases      = ['eV', 'electron-volt']
+      ev.aliases      = %w[eV electron-volt electron_volt]
       ev.definition   = RubyUnits::Unit.new('1.602E-19 joule')
       ev.display_name = 'electron-volt'
     end
@@ -11,31 +11,37 @@ describe "Unit::Definition('eV')" do
 
   describe '#name' do
     subject { super().name }
+
     it { is_expected.to eq('<eV>') }
   end
 
   describe '#aliases' do
     subject { super().aliases }
-    it { is_expected.to eq(%w[eV electron-volt]) }
+
+    it { is_expected.to eq(%w[eV electron-volt electron_volt]) }
   end
 
   describe '#scalar' do
     subject { super().scalar }
+
     it { is_expected.to eq(1.602E-19) }
   end
 
   describe '#numerator' do
     subject { super().numerator }
+
     it { is_expected.to include('<kilogram>', '<meter>', '<meter>') }
   end
 
   describe '#denominator' do
     subject { super().denominator }
+
     it { is_expected.to include('<second>', '<second>') }
   end
 
   describe '#display_name' do
     subject { super().display_name }
+
     it { is_expected.to eq('electron-volt') }
   end
 end
