5de6791a7367df900240fa1c23dbcd561b08ea5e
Kevin Olbrich <kevin.olbrich@gethealthie.com>
2024-03-16 16:54:23 -0400

    Unit format if numerator is 1 in unit faction Fixes #335
    Add `format: :exponential` as an option on `#to_s` and `units`.
    
    This will formate the unit with all units treated like exponentials, ones in the denominator will have negative powers.

--- spec/ruby_units/unit_spec.rb
+++ spec/ruby_units/unit_spec.rb
@@ -2307,14 +2307,17 @@ describe 'Unit Math' do
   end
 end
 
-describe 'Unit Output formatting' do
-  context RubyUnits::Unit.new('10.5 m/s^2') do
-    specify { expect(subject.to_s).to eq('10.5 m/s^2') }
-    specify { expect(subject.to_s('%0.2f')).to eq('10.50 m/s^2') }
-    specify { expect(subject.to_s('%0.2e km/s^2')).to eq('1.05e-02 km/s^2') }
-    specify { expect(subject.to_s('km/s^2')).to eq('0.0105 km/s^2') }
-    specify { expect(subject.to_s(STDOUT)).to eq('10.5 m/s^2') }
-    specify { expect { subject.to_s('random string') }.to raise_error(ArgumentError, "'random' Unit not recognized") }
+describe '#to_s (Unit Output formatting)' do
+  describe '10.5 m/s^2' do
+    subject(:unit) { RubyUnits::Unit.new('10.5 m/s^2') }
+
+    it { expect(unit.to_s).to eq('10.5 m/s^2') }
+    it { expect(unit.to_s(format: :exponential)).to eq('10.5 m*s^-2') }
+    it { expect(unit.to_s('%0.2f')).to eq('10.50 m/s^2') }
+    it { expect(unit.to_s('%0.2e km/s^2')).to eq('1.05e-02 km/s^2') }
+    it { expect(unit.to_s('km/s^2')).to eq('0.0105 km/s^2') }
+    it { expect(unit.to_s(STDOUT)).to eq('10.5 m/s^2') }
+    it { expect { unit.to_s('random string') }.to raise_error(ArgumentError, "'random' Unit not recognized") }
   end
 
   context 'for a unit with a custom display_name' do
