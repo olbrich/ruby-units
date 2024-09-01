d25259d105d08b8daac9f286c9d78de52a1d46f1
Kevin Olbrich <22176+olbrich@users.noreply.github.com>
2024-09-01 13:50:22 -0400

    Issue335-Unit-format-if-numerator-is-1-in-unit-faction (#348)

--- spec/ruby_units/configuration_spec.rb
+++ spec/ruby_units/configuration_spec.rb
@@ -1,28 +1,52 @@
 require 'spec_helper'
 
 describe RubyUnits::Configuration do
-  describe '.separator is true' do
-    it 'has a space between the scalar and the unit' do
-      expect(RubyUnits::Unit.new('1 m').to_s).to eq '1 m'
+  describe '.separator' do
+    context 'when set to true' do
+      it 'has a space between the scalar and the unit' do
+        expect(RubyUnits::Unit.new('1 m').to_s).to eq '1 m'
+      end
+    end
+
+    context 'when set to false' do
+      around do |example|
+        RubyUnits.configure do |config|
+          config.separator = false
+        end
+        example.run
+        RubyUnits.reset
+      end
+
+      it 'does not have a space between the scalar and the unit' do
+        expect(RubyUnits::Unit.new('1 m').to_s).to eq '1m'
+        expect(RubyUnits::Unit.new('14.5 lbs').to_s(:lbs)).to eq '14lbs 8oz'
+        expect(RubyUnits::Unit.new('220 lbs').to_s(:stone)).to eq '15stone 10lbs'
+        expect(RubyUnits::Unit.new('14.2 ft').to_s(:ft)).to eq %(14'2-2/5")
+        expect(RubyUnits::Unit.new('1/2 cup').to_s).to eq '1/2cu'
+        expect(RubyUnits::Unit.new('123.55 lbs').to_s('%0.2f')).to eq '123.55lbs'
+      end
     end
   end
 
-  describe '.separator is false' do
-    around do |example|
-      RubyUnits.configure do |config|
-        config.separator = false
+  describe '.format' do
+    context 'when set to :rational' do
+      it 'uses rational notation' do
+        expect(RubyUnits::Unit.new('1 m/s^2').to_s).to eq '1 m/s^2'
       end
-      example.run
-      RubyUnits.reset
     end
 
-    it 'does not have a space between the scalar and the unit' do
-      expect(RubyUnits::Unit.new('1 m').to_s).to eq '1m'
-      expect(RubyUnits::Unit.new('14.5 lbs').to_s(:lbs)).to eq '14lbs 8oz'
-      expect(RubyUnits::Unit.new('220 lbs').to_s(:stone)).to eq '15stone 10lbs'
-      expect(RubyUnits::Unit.new('14.2 ft').to_s(:ft)).to eq %(14'2-2/5")
-      expect(RubyUnits::Unit.new('1/2 cup').to_s).to eq '1/2cu'
-      expect(RubyUnits::Unit.new('123.55 lbs').to_s('%0.2f')).to eq '123.55lbs'
+    context 'when set to :exponential' do
+      around do |example|
+        RubyUnits.configure do |config|
+          config.format = :exponential
+        end
+        example.run
+        RubyUnits.reset
+      end
+
+      it 'uses exponential notation' do
+        expect(RubyUnits::Unit.new('1 m/s^2').to_s).to eq '1 m*s^-2'
+      end
     end
   end
 end
